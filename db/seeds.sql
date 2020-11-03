a1155b2d705b46c6da7267a73b59a223ad7053bf
Charlie Howard <chuckhoward99@gmail.com>
2020-11-03 14:35:54 -0700

    update

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ("bacon-cheeseburger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES ("cheese-burger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES ("veggie-burger", true);
